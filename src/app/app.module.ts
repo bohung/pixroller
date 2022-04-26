228f6bba19ce25ea20fb2965a695b838a7de5023
shelley <shelley.lin@pixroller.com>
2022-04-26 18:26:26 +0800

    0426 ng new practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Template1Component } from './template1/template1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Template1Component
   ],
   imports: [
     BrowserModule,
