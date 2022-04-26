12fcd929f0d7f3896d1868d9c0b4a937c3409306
kujojolala <lala.yang@pixroller.com>
2022-04-26 15:18:37 +0800

    lala0426test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Lalatest0426Component } from './lalatest0426/lalatest0426.component';
+import { Lala0426Component } from './lala0426/lala0426.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Lalatest0426Component,
+    Lala0426Component
   ],
   imports: [
     BrowserModule,
