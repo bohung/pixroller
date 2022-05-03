4ae97516f708f982421b92ec8bfb3379fe17edff
kujojolala <lala.yang@pixroller.com>
2022-05-03 22:38:19 +0800

    0503mergemaster

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Template1Component } from './template1/template1.component';
 
+import {SafePipe} from './safe.pipe';
+import { DatabindingExampleComponent } from './databinding-example/databinding-example.component'
+
 @NgModule({
   declarations: [
     AppComponent,
-    Template1Component
+    Template1Component,
+    DatabindingExampleComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
