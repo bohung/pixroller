936ece1b8b14077b4c6070f915eb651685a1e28f
shuang <dr39e7am21@gmail.com>
2022-05-05 23:58:17 +0800

    homework example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppComponent } from './app.component';
 import {SafePipe} from './safe.pipe';
 import { DatabindingExampleComponent } from './databinding-example/databinding-example.component';
 import { DatabindingPractice1Component } from './databinding-practice1/databinding-practice1.component'
+import { LTRMPDEXLECA22000Component } from './render/template/LTR_MPD_EXL_ECA22_1_000/ltrmpdexleca22000.component';
+import { RenderComponent } from './render/render.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DatabindingExampleComponent,
     SafePipe,
-    DatabindingPractice1Component
+    DatabindingPractice1Component,
+    LTRMPDEXLECA22000Component,
+    RenderComponent,
   ],
   imports: [
     BrowserModule,
