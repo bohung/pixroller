2b3df95039ae340929cb7ea3b4a7b75a696d666a
shuang <dr39e7am21@gmail.com>
2022-05-05 14:25:04 +0800

    0504未整理的教材

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {SafePipe} from './safe.pipe';
-import { DatabindingExampleComponent } from './databinding-example/databinding-example.component'
+import { DatabindingExampleComponent } from './databinding-example/databinding-example.component';
+import { DatabindingPractice1Component } from './databinding-practice1/databinding-practice1.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     DatabindingExampleComponent,
-    SafePipe
+    SafePipe,
+    DatabindingPractice1Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/databinding-practice1/databinding-practice1.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-databinding-practice1',
+  templateUrl: './databinding-practice1.component.html',
+  styleUrls: ['./databinding-practice1.component.css']
+})
+export class DatabindingPractice1Component implements OnInit {
+  light = "電燈"
+
+  innerHTMLbinding = "<sup style='color:red'> fighfig </sup>";
+
+  targetPerson:any = {
+    pdfName:"aaa"
+  }
+
+  fruits = [
+    'apple',
+    'banana'
+  ]
+  
+  apple = "red"
+  appleObj ={
+    'color':this.apple === 'green' ? 'green' : 'red'
+  }
+  appleObj2 ={
+    'appleGreen' : this.apple === 'green',
+    'appleRed' : this.apple === 'red'
+  }
+
+  //陣列就放包含了所有房間
+  //網頁則是要展示一次只展現一個房間
+  drugs = [
+    {
+      "F_Service_Dte": '12/05/2021',
+      "F_Rx_Nbr": "000007527071",
+      "F_Drug_Nme": 'LOSARTAN POTASSIUM',
+      "F_Net_Check_Diff_Amt": '-6.43',
+      "F_Explanation_Codes_Txt": "AC[]A prior claim reversal caused your original claim to be reversed and repriced",
+    }, {
+      "F_Service_Dte": "12/05/2021",
+      "F_Rx_Nbr": "000007542128",
+      "F_Drug_Nme": "METOPROLOL SUCCINATE",
+      "F_Net_Check_Diff_Amt": "-2.59",
+      "F_Explanation_Codes_Txt": "AC[]A prior claim reversal caused your original claim to be reversed and repriced",
+    }
+  ]
+
+  //1.路徑存成變數
+  //2.html標籤的屬性加入中括號，才能順利將資料導入網頁
+  imgVar = '../../assets/LOG_2013_TNLA_1_000.png';
+
+  open = false
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
