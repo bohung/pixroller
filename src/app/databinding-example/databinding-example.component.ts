d8fb6aa12e78812055a645ce31fa73205ba3892f
anniechang1989 <annie.chang@pixroller.com>
2022-05-06 14:07:46 +0800

    annie0506

--- src/app/databinding-example/databinding-example.component.ts
+++ src/app/databinding-example/databinding-example.component.ts
@@ -12,6 +12,38 @@ export class DatabindingExampleComponent implements OnInit {
 
   innerHTMLbinding = "<sup style='color:red'> fighfig </sup>";
 
+  imgMH = '../../assets/myohan.jpg';
+
+  drugs = [
+    {
+      "F_Service_Dte": '12/05/2021',
+      "F_Rx_Nbr": "000007527071",
+      "F_Drug_Nme": 'LOSARTAN POTASSIUM',
+      "F_Net_Check_Diff_Amt": '-6.43',
+      "F_Explanation_Codes_Txt": "AC[]A prior claim reversal caused your original claim to be reversed and repriced",
+    }, {
+      "F_Service_Dte": "12/05/2021",
+      "F_Rx_Nbr": "000007542128",
+      "F_Drug_Nme": "METOPROLOL SUCCINATE",
+      "F_Net_Check_Diff_Amt": "-2.59",
+      "F_Explanation_Codes_Txt": "AC[]A prior claim reversal caused your original claim to be reversed and repriced",
+    }
+  ]
+
+  apple = "000";
+
+  rose = "blue";
+
+  roseObj = {
+    'color':this.rose === 'blue' ? 'blue' : 'white'
+  }
+  
+  go = "black";
+
+  goObj = {
+    '.goBlack':this.go === 'black',
+    '.goWhite':this.go === 'white'
+  } 
 
   constructor() { }
 
