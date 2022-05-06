3fd28b0b7ac51045563a75db60350943e0022c5b
oonahuang <oona.huang@pixroller.com>
2022-05-06 12:06:09 +0800

    databindingofngfor

--- src/app/databinding-practice/databinding-practice.component.ts
+++ src/app/databinding-practice/databinding-practice.component.ts
@@ -6,14 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./databinding-practice.component.css']
 })
 export class DatabindingPracticeComponent implements OnInit {
-  light = "燈"
-  number = 123
+  light="燈"
+  number=123
 
   innerHTMLbinding="<sup style='color:red'> fighfig </sup>"
 
-  targetPerson: any = {
-    pdfName:"aaa"
+  targetPerson:any={
+    '2pdfName':"aaa"
   }
+
+  rooms=['a','b','c']
+  doors=['1','2','3']
+
+  drugs = [
+    {
+      "F_Service_Dte": '12/05/2021',
+      "F_Rx_Nbr": "000007527071",
+      "F_Drug_Nme": 'LOSARTAN POTASSIUM',
+      "F_Net_Check_Diff_Amt": '-6.43',
+      "F_Explanation_Codes_Txt": "AC[]A prior claim reversal caused your original claim to be reversed and repriced",
+    }, {
+      "F_Service_Dte": "12/05/2021",
+      "F_Rx_Nbr": "000007542128",
+      "F_Drug_Nme": "METOPROLOL SUCCINATE",
+      "F_Net_Check_Diff_Amt": "-2.59",
+      "F_Explanation_Codes_Txt": "AC[]A prior claim reversal caused your original claim to be reversed and repriced",
+    }
+  ]
+
+  imgVar='../../assets/LOG_6627_EM2016_1_000.png'
 
   constructor() { }
 
